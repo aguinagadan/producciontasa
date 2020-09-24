4e56e95c08b680a46a9874a28a60ca0ddaeb88e5
Samuel Rojas <samuel.rojas@globant.com>
2020-09-24 14:27:44 -0500

    [#5] dashboard put back php 2

--- theme/remui/layout/mydashboard.php
+++ theme/remui/layout/mydashboard.php
@@ -27,5 +27,112 @@ defined('MOODLE_INTERNAL') || die();
 
 require_once('common.php');
 
+use moodle_url;
+use block_xp\local\xp\level_with_name;
+use block_xp\local\xp\level_with_badge;
+use core_completion\progress;
+use core_course_renderer;
+use coursecat_helper;
+
+global $USER;
+
+function getLevelPropertyValue($level, $property) {
+	$returnedValue = '';
+
+	switch($property) {
+		case 'name':
+			$name = $level instanceof level_with_name ? $level->get_name() : null;
+			if (empty($name)) {
+				$name = get_string('levelx', 'block_xp', $level->get_level());
+			}
+			$returnedValue = $name;
+			break;
+	}
+	return $returnedValue;
+}
+
+function getLevelBadge($level) {
+	$levelnum = $level->get_level();
+	$classes = 'block_xp-level level-' . $levelnum;
+	$label = get_string('levelx', 'block_xp', $levelnum);
+	$classes .= ' d-badge';
+
+	$html = '';
+	if ($level instanceof level_with_badge && ($badgeurl = $level->get_badge_url()) !== null) {
+		$html .= html_writer::tag(
+			'div',
+			html_writer::empty_tag('img', ['src' => $badgeurl,
+				'alt' => $label, 'class'=> 'd-badge-img']),
+			['class' => $classes . ' level-badge', 'style' => 'height: 75px;']
+		);
+	} else {
+		$html .= html_writer::tag('div', $levelnum, ['class' => $classes, 'aria-label' => $label]);
+	}
+	return $html;
+}
+
+/**
+ * Format an amount of XP.
+ *
+ * @param int $amount The XP.
+ * @return string
+ */
+function xp($amount) {
+	$xp = (int) $amount;
+	if ($xp > 999) {
+		$thousandssep = get_string('thousandssep', 'langconfig');
+		$xp = number_format($xp, 0, '.', $thousandssep);
+	}
+	$o = '';
+	$o .= html_writer::start_div('block_xp-xp');
+	$o .= html_writer::div($xp, 'pts');
+	$o .= html_writer::div('xp', 'sign sign-sup');
+	$o .= html_writer::end_div();
+	return $o;
+}
+
+/**
+ * Returns the progress bar rendered.
+ *
+ * @param state $state The renderable object.
+ * @param bool $showpercentagetogo Show the percentage to go.
+ * @return string HTML produced.
+ */
+function getProgressBar($state) {
+	$classes = ['block_xp-level-progress', 'd-level-progress'];
+	$pc = $state->get_ratio_in_level() * 100;
+	if ($pc != 0) {
+		$classes[] = 'progress-non-zero';
+	}
+
+	$html = '';
+
+	$html .= html_writer::start_tag('div', ['class' => implode(' ', $classes)]);
+
+	$html .= html_writer::start_tag('div', ['class' => 'xp-bar-wrapper d-progress-bar-level', 'role' => 'progressbar',
+		'aria-valuenow' => round($pc, 1), 'aria-valuemin' => 0, 'aria-valuemax' => 100]);
+	if($pc == 0) {
+		$xpBar = 'd-xp-bar-0';
+	} else {
+		$xpBar = 'd-xp-bar';
+	}
+
+	$html .= html_writer::tag('div', '', ['style' => "width: {$pc}%;", 'class' => 'xp-bar '.$xpBar]);
+	$html .= html_writer::end_tag('div');
+	$html .= html_writer::end_tag('div');
+	return $html;
+}
+
+function convertDateToSpanish($timestamp) {
+	setlocale(LC_TIME, 'es_ES', 'Spanish_Spain', 'Spanish');
+	return strftime("%d de %B de %Y", $timestamp);
+}
+
+function getCategoryById($catId) {
+	global $DB;
+	return $DB->get_record('course_categories',array('id'=>$catId));
+}
+
+
 echo $OUTPUT->render_from_template('theme_remui/mydashboard', $templatecontext);
 
