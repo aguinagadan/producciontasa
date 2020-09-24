c00fd0e8133ff68e407bace4466aaa017a7b9bb0
Samuel Rojas <samuel.rojas@globant.com>
2020-09-24 18:06:17 -0500

    [#3] terminologia fix

--- local/terminologia/index.php
+++ local/terminologia/index.php
@@ -4,6 +4,12 @@ require_once 'classes/controller/Termino.php';
 require_once(dirname(__FILE__) . '/../../config.php');
 use LocalPages\Controller\Termino as TerminoController;
 
+$title = 'Aula virtual - Terminología';
+// Set up the page.
+$url = new moodle_url("/local/terminologia/index.php", array('component' => $component, 'search' => $search));
+$PAGE->set_title($title);
+$PAGE->set_url($url);
+
 try {
 	error_reporting(E_ALL);
 
@@ -14,12 +20,6 @@ try {
 	if (!has_capability('tool/policy:managedocs', $personalcontext)) {
 		$isManager = 0;
 	}
-
-	$title = 'Aula virtual - Terminología';
-	// Set up the page.
-	$url = new moodle_url("/local/terminologia/index.php", array('component' => $component, 'search' => $search));
-	$PAGE->set_title($title);
-	$PAGE->set_url($url);
 
 	$PAGE->requires->css(new moodle_url('css/terminologia.css'));
 
