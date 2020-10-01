a5c34fbec88e971df688522cf79b95e96666bd20
Samuel Rojas <samuel.rojas@globant.com>
2020-10-01 00:41:13 -0500

    [#5] dashboard seguimiento fix ajax

--- my/ajax_controller_seguimiento.php
+++ my/ajax_controller_seguimiento.php
@@ -301,7 +301,7 @@ function get_personal_detail() {
 	foreach($personalArr as $personal) {
 		$returnHTML.= '<div zona-name="'. $tipoPersonal . $division . $zona .'" course-id="'. $courseId .'" class="ss-container ss-main-container-personal row ss-m-b-05">
 											<input class="personaIds" type="hidden" value="'.$personal['id'].'">
-											<div data-id="'. $personal['id'] . '" data-val="'.strtoupper($personal['firstname']).'" class="col-sm personal-clickable" style="cursor: pointer; font-size: 18px;" data-toggle="modal" data-target="#myModal">'. $personal['firstname'] . $personal['lastname'] . '</div>
+											<div data-id="'. $personal['id'] . '" data-val="'.strtoupper($personal['firstname']).'" class="col-sm personal-clickable" style="cursor: pointer; font-size: 18px;" data-toggle="modal" data-target="#myModal">'. $personal['firstname'] . ' ' . $personal['lastname'] . '</div>
 											<div data-id="'. $personal['id'] . '" data-val="'.strtoupper($personal['firstname']).'" class="col-xs logo-mail-clickable" data-toggle="modal" data-target="#myModal" style="cursor: pointer;">
 													<img src="../theme/remui/pix/ic_email_24px.png">
 											</div>';
