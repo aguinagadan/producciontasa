42df8a2393b930b52800cf67208ce702ab233829
Samuel Rojas <samuel.rojas@globant.com>
2020-10-09 19:24:11 -0500

    [#9] sincronizacion

--- local/systemscripts/lib.php
+++ local/systemscripts/lib.php
@@ -133,6 +133,7 @@ function getADUsers($key, $skipToken='') {
 	return $responseData;
 }
 function updateUser($user, $userAD) {
+	global $DB;
 
 	$dni           = !empty($userAD['extension_f356ba22a23b4c2fb35162e63d13246c_userDocumentNumber']) ? $userAD['extension_f356ba22a23b4c2fb35162e63d13246c_userDocumentNumber'] : '';
 
@@ -162,4 +163,10 @@ function updateUser($user, $userAD) {
 	$user->profile_field_posicion = $posicion;
 
 	profile_save_data($user);
+
+	$user->firstname = $userAD['givenName'];
+	$user->lastname = $userAD['surname'];
+	$user->email = !empty($userAD['mail']) ? $userAD['mail'] : $userAD['otherMails'][0];
+
+	$DB->update_record('user', $user);
 }
