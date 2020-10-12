efedb6a66c0b3cb0eea27cd541c9d5fa4d570cdc
Samuel Rojas <samuel.rojas@globant.com>
2020-10-11 21:23:28 -0500

    [#10] excel

--- my/testGrade.php
+++ my/testGrade.php
@@ -4,16 +4,34 @@ require_once(dirname(__FILE__) . '/../config.php');
 
 use moodle_url;
 
-global $USER, $CFG;
+global $USER, $CFG, $DB;
 
 require_once($CFG->dirroot . '/lib/gradelib.php');
 require_once($CFG->dirroot . '/enrol/externallib.php');
 require_once($CFG->dirroot. '/course/lib.php');
 require_once($CFG->dirroot . '/user/profile/lib.php');
 
-$inicial = grade_get_grades(10, 'mod', 'quiz', 1, 27);
-$final = grade_get_grades(10, 'mod', 'quiz', 2, 27);
+$quiz = $DB->get_records_sql("select * from {quiz} q where q.course = ?", array(10));
+$courseCompletion = $DB->get_records_sql("select * from {course_completions} c where c.course = ? and c.userid = ?", array(10,27));
+$quizIdInicio = array_shift($quiz);
+$quizIdFin = end($quiz);
 
+$inicial = grade_get_grades(10, 'mod', 'quiz', $quizIdInicio->id, 27);
+$final = grade_get_grades(10, 'mod', 'quiz', $quizIdFin->id, 27);
+
+$inicial = $inicial->items[0]->grades[4]->grade ? $inicial->items[0]->grades[4]->grade : '-';
+$final = $final->items[0]->grades[4]->grade ? $final->items[0]->grades[4]->grade : '-';
+
+$timeCompleted = array_shift($courseCompletion)->timecompleted;
+$timeCompleted = $timeCompleted != NULL ? date('d/m/Y', $timeCompleted) : '-';
+
+if($inicial != '-' && $final != '-' && $timeCompleted != '-') {
+	$cumplimiento = 1;
+} else {
+	$cumplimiento = 0;
+}
 var_dump($inicial);
 var_dump($final);
+var_dump($timeCompleted);
+var_dump($cumplimiento);
 exit;
