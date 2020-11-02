24db6f768be766f04f73d8ddd4bcbbc4fd0b5a49
Samuel Rojas <samuel.rojas@globant.com>
2020-11-01 23:05:01 -0500

    [#15] download files

--- mod/customcert/view.php
+++ mod/customcert/view.php
@@ -181,6 +181,6 @@ if (!$downloadown && !$downloadissue) {
 
     // Now we want to generate the PDF.
     $template = new \mod_customcert\template($template);
-    $template->generate_pdf(false, $userid, $course);
+    $template->generate_pdf(false, $userid, false, $course);
     exit();
 }
