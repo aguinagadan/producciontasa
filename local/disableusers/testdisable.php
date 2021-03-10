ebbdc16d5da40eb9e26d52f0b401d93aef7738d4
Samuel Rojas <samuel.rojas@globant.com>
2021-03-10 01:01:36 -0500

    updates

--- local/disableusers/testdisable.php
+++ local/disableusers/testdisable.php
@@ -6,6 +6,10 @@ var_dump('a');
 require_once(dirname(__FILE__) . '/../../config.php');
 require_once($CFG->dirroot.'/user/profile/lib.php');
 
+var_dump('b');
+exit;
+
+
 function execCurlD($data) {
 	$curl = curl_init();
 
