1b81a1db9f27aebfba531733ccef53324735e076
Samuel Rojas <samuel.rojas@globant.com>
2021-03-10 00:32:42 -0500

    updates

--- local/disableusers/testdisable.php
+++ local/disableusers/testdisable.php
@@ -103,19 +103,16 @@ $users = $DB->get_records('user');
 $indicadorDeNombreUsuario = 'tasa.com';
 
 foreach($users as $user) {
-	if(
+	//if(
 		(strpos(strtolower($user->username), $indicadorDeNombreUsuario) !== false && !in_array(strtolower($user->username), $usersAdArr))
 		|| (isset($userAD['extension_f356ba22a23b4c2fb35162e63d13246c_userEndDate']) &&
 			isLowerThanToday($userAD['extension_f356ba22a23b4c2fb35162e63d13246c_userEndDate']))
-	) {
-		var_dump($userAD['extension_f356ba22a23b4c2fb35162e63d13246c_userEndDate']);
-		exit;
+	//) {
+	if($user->id == 4681) {
 		$userMainDataObj = new stdClass();
 		$userMainDataObj->id = $user->id;
 		$userMainDataObj->deleted = 1;
 		$DB->update_record('user', $userMainDataObj);
-	} else {
-		var_dump('test');
 	}
 }
 exit;
