b6dc81c2f4b70b9aa861832fbe244ce29a9da6e4
Samuel Rojas <samuel.rojas@globant.com>
2021-03-10 01:00:30 -0500

    updates

--- local/disableusers/testdisable.php
+++ local/disableusers/testdisable.php
@@ -6,5 +6,33 @@ var_dump('a');
 require_once(dirname(__FILE__) . '/../../config.php');
 require_once($CFG->dirroot.'/user/profile/lib.php');
 
-var_dump('b');
-exit;
+function execCurlD($data) {
+	$curl = curl_init();
+
+	$url = $data['url'];
+	$postFields = $data['postFields'];
+	$httpHeader = $data['httpHeader'];
+	$httpMethod = $data['httpMethod'];
+
+	$curlSetOptArray = array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => $httpMethod
+	);
+
+	if($httpMethod == 'POST') {
+		$curlSetOptArray[CURLOPT_POSTFIELDS] = $postFields;
+	}
+	$curlSetOptArray[CURLOPT_HTTPHEADER] = $httpHeader;
+
+	curl_setopt_array($curl, $curlSetOptArray);
+	$response = curl_exec($curl);
+	curl_close($curl);
+	$responseData = json_decode($response,true);
+	return $responseData;
+}
